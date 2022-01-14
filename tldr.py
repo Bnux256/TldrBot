cc0e739eecea9a049a050e7c0ee89929e02445a8
Barack K <barackk321@gmail.com>
2022-01-14 08:37:37 +0200

    adding update cache function

--- tldr.py
+++ tldr.py
@@ -1,4 +1,34 @@
-import request
+import requests
+import glob
+from zipfile import ZipFile
+import os
+
 '''
 gets zip file - unzips it. create update command. return markdown file or None.  
 '''
+
+
+def update_cache():
+    # Downloading Cache
+    zipUrl = "https://raw.githubusercontent.com/tldr-pages/tldr-pages.github.io/master/assets/tldr.zip"
+    print('Downloading files, please wait')
+    zipFile = requests.get(zipUrl)
+    print('aaa')
+    with open('tldr.zip', 'wb') as file:
+        file.write(zipFile.content)
+    print('Download complete')
+
+    # Unzipping the file
+    print('Unzipping file')
+    os.mkdir('tldr')
+    with ZipFile('tldr.zip', 'r') as zipFile:
+        zipFile.extractall('tldr')
+
+
+def getMdFile(command):
+    """
+
+    """
+
+
+update_cache()
