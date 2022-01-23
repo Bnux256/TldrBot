60cbe0d6c4417162996bf9889ed4701517447c24
Bnux256 <barackk321@gmail.com>
2022-01-23 17:09:02 +0200

    bug fix in update cache

---
+++ tests/test_get_md.py
@@ -0,0 +1,20 @@
+import unittest
+import sys
+# sys.path.append("../tldr")
+import os
+print(os.getcwd())
+from tldr.tldr import get_md
+
+class getMd(unittest.TestCase):
+    def get_en_common(self):
+        self.assertIsNotNone(tldr.get_md('cd'))
+    def get_fr_common(self):
+        self.assertIsNotNone(tldr.get_md('cd', language='fr'))
+    def get_fr_linux(self):
+        self.assertIsNotNone(tldr.get_md('cd', 'linux', 'fr'))
+    def get_not_exist(self):
+        self.assertIsNone(tldr.get_md('aaa','linux', 'fr'))
+
+
+if __name__ == '__main__':
+    unittest.main()
