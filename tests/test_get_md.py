05b53abc4d96e3cd5885043c57ea37c5aff38f0a
Bnux256 <barackk321@gmail.com>
2022-01-24 09:33:56 +0200

    add unittest

--- tests/test_get_md.py
+++ tests/test_get_md.py
@@ -6,13 +6,13 @@ from tldr.tldr import get_md
 
 class getMd(unittest.TestCase):
     def get_en_common(self):
-        self.assertIsNotNone(tldr.get_md('cd'))
+        self.assertIsNotNone(tldr.get_md('cd'), "en_common failed")
     def get_fr_common(self):
-        self.assertIsNotNone(tldr.get_md('cd', language='fr'))
+        self.assertIsNotNone(tldr.get_md('cd', language='fr'), "fr_common failed")
     def get_fr_linux(self):
-        self.assertIsNotNone(tldr.get_md('cd', 'linux', 'fr'))
+        self.assertIsNotNone(tldr.get_md('cd', 'linux', 'fr'), "fr_linux failed")
     def get_not_exist(self):
-        self.assertIsNone(tldr.get_md('aaa','linux', 'fr'))
+        self.assertIsNone(tldr.get_md('aaa','linux', 'fr'), "shouldn't have failed")
 
 
 if __name__ == '__main__':
