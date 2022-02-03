72470770e79b46dbf46ccc008496665e8c89ab86
Barack K <barackk321@gmail.com>
2022-02-03 23:46:26 +0200

    path fixes

--- main.py
+++ main.py
@@ -14,30 +14,32 @@ bot = commands.Bot(
     test_guilds=[866375498762551317]
 )
 
+
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user} (ID: {bot.user.id})")
     print("------")
 
-    
+
 @bot.slash_command(description="Update the bot's TLDR Pages cache")
 async def update(inter):
     await asyncio.run(tldr.update_cache())
     await inter.response.send_message('Cache updated!')
 
+
 # test
 @bot.slash_command(description="Responds with 'World'")
 async def hello(inter, command):
-    await inter.response.send_message(hello(command))
+    a = tldr.temp()
+    await inter.response.send_message(a)
 
-def hello(command):
-    print('aaa')
-    return tldr.get_md(command)
-#test     
+
+# test
 
 @bot.slash_command(description="Retrieve the TLDR for a command")
 async def tldr(inter, command, platform: str = "common", language: str = "None"):
     await inter.response.send_message(tldr.get_md(command, platform, language))
+
 
 try:
     bot.run(TOKEN)
--- tests/test_get_md.py
+++ tests/test_get_md.py
@@ -1,19 +1,21 @@
 import unittest
 import sys
 import os
-sys.path.append(os.path.join(os.getcwd(), r'tldr'))
-import tldr
+import tldr.tldr as tldr
 
 
-class testGetMd(unittest.TestCase):
+class TestGetMd(unittest.TestCase):
     def test_get_en_common(self):
         self.assertIsNotNone(tldr.get_md('cd'), "en_common failed")
+
     def test_get_fr_common(self):
         self.assertIsNotNone(tldr.get_md('cd', language='fr'), "fr_common failed")
+
     def test_get_fr_linux(self):
         self.assertIsNotNone(tldr.get_md('ip', 'linux', 'fr'), "fr_linux failed")
+
     def test_get_not_exist(self):
-        self.assertIsNone(tldr.get_md('aaa','linux', 'fr'), "shouldn't have failed")
+        self.assertIsNone(tldr.get_md('aaa', 'linux', 'fr'), "shouldn't have failed")
 
 
 if __name__ == '__main__':
